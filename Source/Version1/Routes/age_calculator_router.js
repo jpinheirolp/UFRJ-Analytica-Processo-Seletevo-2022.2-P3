43ef289a59b49701d58b9120c19b2b2466fd77ea
João Pinheiro <jpinheirolp@gmail.com>
2022-07-09 20:52:58 -0300

    adequando api ao que foi proposto

--- Source/Version1/Routes/age_calculator_router.js
+++ Source/Version1/Routes/age_calculator_router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controler = require('../../controllers/age_calculator_controller');
 
 router.post('/', controler.calculate_age_date);
-router.get('/', controler.returns_today_date);
+//router.get('/', controler.returns_today_date);
 
 module.exports = router;
